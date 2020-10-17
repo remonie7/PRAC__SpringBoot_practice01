2901da7d4078163c645853aad70ed7db8f5d6c87
remonie7 <remonie7@gmail.com>
2020-10-17 23:11:28 +0900

    create project & pom.xml initial setting

---
+++ src/main/java/net/webprac1/Webprac1Application.java
@@ -0,0 +1,13 @@
+package net.webprac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webprac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webprac1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/webprac1/Webprac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.webprac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webprac1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
