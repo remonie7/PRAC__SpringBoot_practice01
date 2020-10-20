f3a1fddd73d07e04a01a42b3f93b40019f423ef6
remonie7 <remonie7@gmail.com>
2020-10-20 16:05:19 +0900

    로그아웃 구현

--- src/main/java/net/webprac1/web/UserController.java
+++ src/main/java/net/webprac1/web/UserController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +50,12 @@ public class UserController {
 		}
 		System.out.println("로그인 성공");
 		session.setAttribute("user", user);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("user");
 		return "redirect:/";
 	}
 }
