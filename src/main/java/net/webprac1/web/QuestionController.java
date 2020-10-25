8dd5112b8783b8f9a69e7ccb33101233966f6080
remonie7 <remonie7@gmail.com>
2020-10-25 22:58:07 +0900

    글 상세보기 기능 구현

--- src/main/java/net/webprac1/web/QuestionController.java
+++ src/main/java/net/webprac1/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -41,6 +43,14 @@ public class QuestionController {
 		Question question = new Question(sessionUser, title, contents);
 		questionRepository.save(question);
 		return "redirect:/";
+	}
+	
+	
+	@GetMapping("/show/{questionId}")
+	public String show(@PathVariable Long questionId, Model model) {
+		Question question = questionRepository.findById(questionId).get();
+		model.addAttribute("selectQuestion", question);
+		return "qna/show";
 	}
 	
 }
